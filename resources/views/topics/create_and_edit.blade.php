33626864d22597cf374a30a09d19929f34684884
vagrant <vagrant@homestead>
2019-08-17 02:56:58 +0000

    WYSIWYG Editor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="{{asset('css/simditor.css')}}">
+@stop
 @section('content')
 
     <div class="container">
@@ -43,8 +46,9 @@
                                         </select>
                                     </div>
                                     <div class="form-group">
-                                        <textarea name="body" id="body-field" class="form-control" id="editor"
-                                                  rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+                                        <textarea name="body" class="form-control" id="editor"
+                                                  rows="3" placeholder="请填入至少三个字符的内容。"
+                                                  required>{{ old('body', $topic->body ) }}</textarea>
                                     </div>
 
                                     <div class="well well-sm">
@@ -60,3 +64,17 @@
     </div>
 
 @endsection
+
+@section('scripts')
+    <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+    <script>
+        $(document).ready(function(){
+            var editor = new Simditor({
+                textarea: $('#editor'),
+            });
+        });
+    </script>
+@stop
